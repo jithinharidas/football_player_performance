d54ecd3c5e3359baeba8b54dc6ac18b97e69a318
Jithin K Haridas <jithinharidas@Jithins-MacBook-Air.local>
2022-05-21 21:06:54 +0530

    added few lines of code

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ import numpy as np
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1000 * 1000
+
+
+@app.route('/', methods=['POST', 'GET'])
+def homepage():
+    return render_template('index.html')
+
+
+@app.route('/form', methods=['POST', 'GET'])
+def formpage():
+    return render_template('form.html')
+
+
+def transform_and_process_data(df):
+    df.dropna(thresh=5, inplace=True)
+
+    player_fifa_api_id = df['player_fifa_api_id'].unique() 
